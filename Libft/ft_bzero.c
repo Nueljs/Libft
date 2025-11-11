bf9b5f9ef7f95be5087aaf775b848b13ef3f27b9
Manuel Cervera Lara <macerver@c2r10s6.42malaga.com>
2025-11-11 13:57:15 +0100

    dos funciones mas

--- Libft/ft_bzero.c
+++ Libft/ft_bzero.c
@@ -1,37 +1,39 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   ft_memset.c                                        :+:      :+:    :+:   */
+/*   ft_bzero.c                                         :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: macerver <macerver@student.42malaga.com>   +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2025/11/11 11:33:23 by macerver          #+#    #+#             */
-/*   Updated: 2025/11/11 12:52:35 by macerver         ###   ########.fr       */
+/*   Created: 2025/11/11 12:41:13 by macerver          #+#    #+#             */
+/*   Updated: 2025/11/11 13:55:35 by macerver         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-void	*ft_memset(void *s, int c, unsigned int n)
+void ft_bzero(void *s, unsigned int n)
 {
 	unsigned char	*s2;
-	int	i;
-
-	i = 0;
+	
 	s2 = s;
-	while (i < n)
+	while (n > 0)
 	{
-		s2[i] = c;
-		i++;
+		*s2 = '\0';
+		s2++;
+		n--;
 	}
-	return (s);
 }
-/*
+
 #include <stdio.h>
-#include <string.h>
+//#include <string.h>
 int	main(void)
 {
-	char str[7] = "hola 2";
-	ft_memset(str, '~', 3);
-	puts(str);
+	char	mychar[10] = "hola que";
+	int	i;
+	i = 0;
+	ft_bzero(mychar, 3);
+	while (i < 10)
+	{
+		printf("%c", mychar[i++]);
+	}
 	return (0);
-
-}*/
+}
