2393a3a8d915fda958842db88878dc40b7196b8d
Manuel Cervera Lara <macerver@c2r1s3.42malaga.com>
2025-11-24 17:48:45 +0100

    lstlast

--- Libft/ft_lstlast.c
+++ Libft/ft_lstlast.c
@@ -6,7 +6,7 @@
 /*   By: macerver <macerver@student.42malaga.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/11/23 22:39:21 by macerver          #+#    #+#             */
-/*   Updated: 2025/11/23 22:52:26 by macerver         ###   ########.fr       */
+/*   Updated: 2025/11/24 17:30:39 by macerver         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,13 +14,9 @@
 
 t_list	*ft_lstlast(t_list *lst)
 {
-	t_list	*last;
-
-	if (lst -> next == NULL)
-		return (lst);
-	else
-	{
-		last = lst -> next;
-		ft_lstlast(last);
-	}
+	if (!lst)
+		return (NULL);
+	while (lst -> next != NULL)
+		lst = lst -> next;
+	return (lst);
 }
